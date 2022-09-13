dd7e2011d1e940d55fc204b6b9be2cc8a659e423
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-09-13 15:20:50 +0700

    home

--- routes/vendors.js
+++ routes/vendors.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+// id vendor: 1
+// 1_1
+// 1_2
+
 router.get('/vendors', (req, res) => {
   res.send({
     vendor_one: {
@@ -9,13 +13,17 @@ router.get('/vendors', (req, res) => {
           id: 1,
           name: "Bread",
           price: 15000,
-          image_id: "1"
+          image_id: "1",
+          limited_time: true,
+          max_order_time: 2200
         },
         {
           id: 2,
           name: "Bread 2",
           price: 10000,
-          image_id: "2"
+          image_id: "2",
+          limited_time: false,
+          max_order_time: null
         }
       ],
       vendor_image_id: "vendor_one",
@@ -26,5 +34,33 @@ router.get('/vendors', (req, res) => {
     }
   })
 })
+
+router.get('/home', (req, res) => {
+  res.send({
+    products: [
+      {
+        name: 'product 1',
+        vendor_id: '1',
+        price: 5000,
+        limited_time: true,
+        max_order_time: 2200
+      },
+      {
+        name: 'product 3',
+        vendor_id: '1',
+        price: 5000,
+        limited_time: true,
+        max_order_time: 2200
+      },
+      {
+        name: 'product 2',
+        vendor_id: '1',
+        price: 5000,
+        limited_time: true,
+        max_order_time: 2200
+      }
+    ]
+  })
+});
 
 module.exports = router;
